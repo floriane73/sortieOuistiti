f80b1d9491ec06ae2b90d036ad3963b95e25d6b1
Floriane <floriane.debernardi2021@campus-eni.fr>
2021-08-03 15:26:21 +0200

    Modification des controllers main et security pour affichage login en page d'accueil du site

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,19 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
- * @Route("/", name="main_")
+ * @Route("/home", name="main_")
  */
 class MainController extends AbstractController
 {
+
     /**
      * @Route("", name="index")
      */
+
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+
+
     }
 }
