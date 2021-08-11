2d42d3ddc5c4f1528245cfe0ece70d84e4e8be33
masterk <badetken@yahoo.fr>
2021-08-11 13:03:01 +0200

    Ajout fonctionnalité ajout d'un lieu + lien nav

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Lieu;
+use App\Form\LieuType;
+use App\Repository\LieuRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class LieuController extends AbstractController
 {
     /**
      * @Route("/lieu", name="lieu")
      */
-    public function index(): Response
+    public function liste(LieuRepository $lieuRepository): Response
     {
-        return $this->render('lieu/index.html.twig', [
-            'controller_name' => 'LieuController',
+        $lieux =$lieuRepository->findAll();
+       // dd($lieux);
+
+        return $this->render('lieu/liste.html.twig', [
+            'listeLieux' => $lieux
+        ]);
+    }
+
+    /**
+     * @Route("/lieu/ajouter", name="lieu_ajouter")*
+     */
+    public function ajouter(Request $request, EntityManagerInterface $entityManager)
+    {
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+        $lieuForm->handleRequest($request);
+
+        if($lieuForm->isSubmitted() && $lieuForm->isValid()){
+            $entityManager->persist($lieu);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le lieu '.$lieuForm['nom']->getData().' a bien été ajouté !');
+            return  $this->redirectToRoute('lieu');
+        }
+
+        return $this->render('lieu/ajouter.html.twig', [
+            'lieuForm' => $lieuForm->createView()
         ]);
     }
 }
